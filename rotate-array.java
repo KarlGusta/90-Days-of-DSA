b42a3a172f74ad193d075bd6c59b01746182226b
karlGusta <karlgustaesimit@gmail.com>
2021-12-03 20:03:16 +0300

    started the rotate array challenge

--- rotate-array.java
+++ rotate-array.java
@@ -25,8 +25,31 @@ Constraints:
 
 */
 
+/*Borrowing from: https://www.geeksforgeeks.org/array-rotation/*/
+
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        /* Function to left rotate arr[] of size n by d*/
+        void leftRotate(int arr[], int d, int n) {
+            for (int i = 0; i < d; i++){
+                leftRotatebyOne(arr, n);
+            }
+        }
+
+        void leftRotatebyOne(int arr[], int n){
+            int i, temp;
+            temp = arr[0];
+            for (i = 0; i < n - 1; i++) {
+                arr[i] = arr[i + 1];
+                arr[n-1] = temp;
+            }
+        }
+
+        /* Utility function to print an array */
+        void printArray(int arr[], int n) {
+            for (int i = 0; i < n; i++){
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
 }
