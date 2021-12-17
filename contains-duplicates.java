16acab52a21859ff9ac7a6b8386dae27857a7e05
karlGusta <karlgustaesimit@gmail.com>
2021-12-17 14:49:31 +0300

    Contains duplicates solution

--- contains-duplicates.java
+++ contains-duplicates.java
@@ -21,6 +21,14 @@ Constraints:
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (set.contains(val))
+                return true;
+            set.add(val);
+        }
+        return false;
     }
 }
